9c1bda8091be04321208cb5e67d33d942ee4a3c2
Julsssssssssssss <kiev2006julia@yandex.ru>
2026-02-15 19:46:24 +0300

    change url to backend

--- lesson_9/webclient/src/app/services/api/quiz.api.ts
+++ lesson_9/webclient/src/app/services/api/quiz.api.ts
@@ -4,6 +4,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizApi extends Api {
   public getUrl(): string {
-    return "http://localhost:5000/";
+    return "https://localhost:59701/";
   }
 }
